2c7806cee4296d831a6121d12290edbfbc1221f3
alejoacevedo <ale.acevedo@live.com.ar>
2018-08-16 21:02:00 -0300

    Avanzo clase CSV y creo pruebas

---
+++ src/Test/CSVTest.java
@@ -0,0 +1,74 @@
+package Test;
+
+import Excepciones.FaltaIP;
+import Excepciones.FaltaNombre;
+import Excepciones.InformacionDifiereDeHeaderException;
+import app.CSV;
+import app.Timbre;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CSVTest {
+
+    String[] header = {"Nombre", "Apellido", "Edad"};
+
+
+    @Test
+    public void testCrearCSV()throws IOException{
+        String ruta = "testCrearCSV.csv";
+        CSV csvPrueba = new CSV(ruta, header);
+    }
+
+    @Test
+    public void testAbrirCSV()throws IOException{
+        String ruta = "testAbrirCSV.csv";
+        CSV csvPrueba = new CSV(ruta, header);
+        csvPrueba = new CSV(ruta, header);
+    }
+
+    @Test
+    public void testEscrbirCSVNuevo()throws IOException, InformacionDifiereDeHeaderException{
+        String ruta = "testEscribirCSVNuevo.csv";
+        CSV csvPrueba = new CSV(ruta, header);
+        csvPrueba.agregarAlCSV(Arrays.asList("Alejo", "Acevedo", 22));
+        csvPrueba.agregarAlCSV(Arrays.asList("Federico", "Cavazzoli", 23));
+        csvPrueba.agregarAlCSV(Arrays.asList("Facundo", "Varela", 24));
+    }
+
+    @Test
+    public void testEscrbirCSVUsado()throws IOException, InformacionDifiereDeHeaderException{
+        String ruta = "testEscribirCSVUsado.csv";
+        CSV csvPrueba = new CSV(ruta, header);
+        csvPrueba.agregarAlCSV(Arrays.asList("Alejo", "Acevedo", 22));
+        csvPrueba.agregarAlCSV(Arrays.asList("Federico", "Cavazzoli", 23));
+        csvPrueba.agregarAlCSV(Arrays.asList("Facundo", "Varela", 24));
+        csvPrueba = new CSV(ruta, header);
+        csvPrueba.agregarAlCSV(Arrays.asList("Alejo", "Acevedo", 22));
+        csvPrueba.agregarAlCSV(Arrays.asList("Federico", "Cavazzoli", 23));
+        csvPrueba.agregarAlCSV(Arrays.asList("Facundo", "Varela", 24));
+    }
+
+    @Test
+    public void testLeerTodoElCSV() throws IOException, InformacionDifiereDeHeaderException{
+        String ruta = "testLeerTodoElCSV.csv";
+        CSV csvPrueba = new CSV(ruta, header);
+        csvPrueba.agregarAlCSV(Arrays.asList("Alejo", "Acevedo", 22));
+        csvPrueba.agregarAlCSV(Arrays.asList("Federico", "Cavazzoli", 23));
+        csvPrueba.agregarAlCSV(Arrays.asList("Facundo", "Varela", 24));
+        csvPrueba = new CSV(ruta, header);
+        LinkedList<HashMap<String,String>> lectura = csvPrueba.leerTodoElCSV();
+        assertEquals("Alejo",lectura.get(0).get(header[0]));
+        assertEquals("Acevedo",lectura.get(0).get(header[1]));
+        assertEquals("22",lectura.get(0).get(header[2]));
+
+    }
+}
--- src/app/CSV.java
+++ src/app/CSV.java
@@ -1,29 +1,52 @@
 package app;
 
 import Excepciones.InformacionDifiereDeHeaderException;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.*;
+import java.util.*;
 
 
 public class CSV {
+
     private Reader reader;
     private CSVParser csvParser;
-    private FileWriter Writer;
-    void CSV(String ruta,ArrayList<String> header) throws IOException {
+    private Writer writer;
+    private CSVPrinter csvPrinter;
+    private String[] header;
+    private String ruta;
+
+    public CSV(String ruta, String[] header) throws IOException {
+        this.ruta = ruta;
+        this.header = header;
+        try {
+            crearCSV();
+        }catch (FileAlreadyExistsException e){
+            abrirCSV();
+        }
         this.reader = Files.newBufferedReader(Paths.get(ruta));
-        this.Writer = new FileWriter(ruta, true);
-        this.csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader(String.valueOf(header)).withIgnoreHeaderCase().withSkipHeaderRecord().withTrim());
+        this.csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader(header).withIgnoreHeaderCase().withSkipHeaderRecord().withTrim());
+    }
+
+    private void abrirCSV()throws IOException{
+        this.writer = Files.newBufferedWriter(Paths.get(ruta), StandardOpenOption.APPEND);
+        this.csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(header).withSkipHeaderRecord());
+
+
+    }
+
+    private void crearCSV()throws IOException{
+        this.writer = Files.newBufferedWriter(Paths.get(ruta), StandardOpenOption.CREATE_NEW);
+        this.csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader(header));
+
     }
 
     public List<String> verEncabezado() throws IOException {
@@ -33,12 +56,13 @@ public class CSV {
         return lista_encabezado;
     }
 
-    public void agregarAlCSV(String nueva_informacion) throws InformacionDifiereDeHeaderException, IOException {
+    public void agregarAlCSV(List nueva_informacion) throws InformacionDifiereDeHeaderException, IOException {
         //LA LISTA DEBE ESTAR EN EL ORDEN DEL HEADER. Y POSEER LA MISMA CANTIDAD DE ITEMS.
-
-        //Falta testear el header !!!!!! CAREFUL !!!!!!
-        //ALEJO BOTON
-        this.Writer.append(nueva_informacion);
+        if(nueva_informacion.size() != this.header.length){
+            throw new InformacionDifiereDeHeaderException();
+        }
+        this.csvPrinter.printRecord(nueva_informacion);
+        this.csvPrinter.flush();
     }
 
     public boolean borrarDelCSV(LinkedList<String> info_a_borrar) throws InformacionDifiereDeHeaderException{
@@ -47,11 +71,17 @@ public class CSV {
         return false;
     }
 
-    public LinkedList<LinkedList<String>> leerTodoElCSV(){
+    public LinkedList<HashMap<String,String>> leerTodoElCSV(){
         //FALTA IMPLEMENTACION xD.
-        //DEVUELVE UNA LISTA DE LISTAS, CADA ITEM DE LA LISTA CONTIENE UNA LISTA CON CADA VALOR DE LA LINEA.
-        LinkedList<LinkedList<String>> return_list = new LinkedList<LinkedList<String>>();
-        //SI, ME DABA PAJA EL ERROR Y SOLO LA DECLARE PARA NO VERLA EN ROJO.
+        //DEVUELVE UNA LISTA DE LISTAS, CADA ITEM DE LA LISTA CONTIENE UN DICCIONARIO CON CADA VALOR DE LA LINEA.
+        LinkedList<HashMap<String,String>> return_list = new LinkedList<HashMap<String, String>>();
+        for (CSVRecord csvRecord : csvParser){
+            HashMap<String, String> aux = new HashMap<String, String>();
+            for (String key : header){
+                aux.put(key, csvRecord.get(key));
+            }
+            return_list.add(aux);
+        }
         return return_list;
     }
 
